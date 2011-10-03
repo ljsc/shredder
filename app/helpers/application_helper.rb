19e9cb2a3708694e935e8b82f4f418d42ef7b932
Lou Scoras <ljsc@gwu.edu>
2011-10-03 17:42:18 -0400

    Fixing up the html attributes for the JQM ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def table_view_link(path ,&blk)
+    content = capture(&blk)
+    link_to content, path
+  end
+
+  def toolbar_button(text, path, options)
+    config = {}
+    options.each do |k,v|
+      k = k.to_s
+      if %w|icon iconpos direction prefetch rel|.include? k
+        k = 'data-' + k
+      end
+      config[k] = v
+    end
+    link_to text, path, config
+  end
+
+  def toolbar_right_button(text, path, options)
+    options['class'] = 'ui-btn-right'
+    toolbar_button(text, path, options)
+  end
 end
