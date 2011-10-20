186d2fb8cb2e49bcee0e1b36e97287d5c0ab850a
Lou Scoras <ljsc@gwu.edu>
2011-10-19 20:15:08 -0400

    Adding answer migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111004145118) do
+ActiveRecord::Schema.define(:version => 20111020001237) do
+
+  create_table "answers", :force => true do |t|
+    t.string   "body"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", :force => true do |t|
     t.string   "title"
